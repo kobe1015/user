3c47a1a377930556aa2605d0470d6c1105010fbb
kobe <dragon1015@gmail.com>
2020-12-09 00:49:09 +0900

    Jpa, HikariCP, Mysql 설정

--- src/main/java/com/toris/user/UserApplication.java
+++ src/main/java/com/toris/user/UserApplication.java
@@ -2,9 +2,23 @@ package com.toris.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EntityScan(basePackages = {"com.toris.user.model.entity"})
+@EnableJpaRepositories(basePackages = {"com.toris.user.repository"})
 public class UserApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
